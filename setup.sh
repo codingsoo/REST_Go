9c1109a2c9296c43f075bfc5fab2ad739b10e4ce
Myeongsoo Kim <mkim754@gatech.edu>
2022-05-07 07:54:04 +0000

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,102 @@
+# sudo apt-get update && sudo apt-get install -y git && git clone https://github.com/codingsoo/REST-Go.git
+
+# Install Common Utilities
+sudo apt-get install -y software-properties-common \
+unzip wget gcc git vim libcurl4-nss-dev tmux
+
+# Install Java8
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y maven gradle
+sudo apt-get install -y openjdk-11-jdk
+
+# Install Python3
+sudo apt-get install -y python3-pip python3-virtualenv
+virtualenv venv
+
+# Install NodeJS and NPM
+sudo apt-get install -y nodejs npm
+
+# Install Docker
+sudo apt-get install -y docker.io
+
+# Install Dotnet SDK 5.0
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb \
+&& sudo dpkg -i packages-microsoft-prod.deb \
+&& rm packages-microsoft-prod.deb \
+&& sudo apt-get update \
+&& sudo apt-get install -y apt-transport-https \
+&& sudo apt-get install -y dotnet-sdk-5.0
+
+# Install Ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update && sudo apt-get install -y ethereum
+
+######RESTful Service#####
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+cd ./services/evo_jdk8 && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../jdk8/erc20-rest-service && chmod +x gradlew && ./gradlew build
+cd ../genome-nexus && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../person-controller && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../problem-controller && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../rest-study && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../spring-batch-rest && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../spring-boot-sample-app && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../user-management && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+cd ../../jdk11/cwa-verification && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../market && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../project-tracking-system && mvn clean install -DskipTests && mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+cd ../../..
+
+sudo docker pull genomenexus/gn-mongo
+sudo docker pull mongo
+sudo docker pull mysql
+
+######Testing Tools#####
+# Install Dredd 14.1.0
+sudo npm install -g dredd
+
+# Install EvoMaster 1.3.0
+wget https://github.com/EMResearch/EvoMaster/releases/download/v1.3.0/evomaster.jar.zip
+unzip evomaster.jar.zip
+rm evomaster.jar.zip
+
+. venv/bin/activate
+
+# Install Schemathesis 3.11.6
+pip install schemathesis
+
+# Install APIFuzzer 0.9.11
+cd APIFuzzer && pip install .
+cd ..
+
+# Install RESTler 8.3.0
+wget https://github.com/microsoft/restler-fuzzer/archive/refs/tags/v8.3.0.tar.gz \
+&& tar -xvf v8.3.0.tar.gz \
+&& rm v8.3.0.tar.gz \
+&& mv restler-fuzzer-8.3.0 restler \
+&& cd restler \
+&& mkdir restler_bin \
+&& python ./build-restler.py --dest_dir ./restler_bin
+cd ..
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Install RESTest 1.2.0 (need to install dependency as well)
+cd ./RESTest && sh ./scripts/install_dependencies.sh && mvn clean install -DskipTests
+cd ..
+
+# Install bBOXRT (Commit e5d329133d51aa75cd39209590cac7046d0640b1)
+cd ./bBOXRT && mvn install -DskipTests
+cd ..
+
+# Install RestTestGen
+cd ./RestTestGen && chmod +x gradlew && ./gradlew jar
+cd ..
+
+wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.agent/0.8.7/org.jacoco.agent-0.8.7-runtime.jar
+wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.cli/0.8.7/org.jacoco.cli-0.8.7-nodeps.jar
+
