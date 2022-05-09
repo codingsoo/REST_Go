351f4cc0281c19933ea005523603c5ad5b68fa39
Myeongsoo Kim <mkim754@Administrators-MacBook-Pro.local>
2022-05-09 09:41:30 -0400

    Add coverage collector for small

---
+++ small_cov.sh
@@ -0,0 +1,9 @@
+NO_INTERVAL=6
+TIME_INTERVAL=30
+
+SET=$(seq 1 "$NO_INTERVAL")
+for i in $SET
+do
+    sleep "$TIME_INTERVAL"s
+    java -jar org.jacoco.cli-0.8.7-nodeps.jar dump --address localhost --port "$1" --destfile jacoco_"$1"_"$i".exec
+done
